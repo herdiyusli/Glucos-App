c288dd5da2ba85d13459076d9593217a2e214efc
herdi34 <herdi13yyo@gmail.com>
2022-05-18 22:41:10 +0700

    create custom view

---
+++ GlucosApp/app/src/main/java/com/herdi/yusli/glucosapp/customeView/EmailEditText.kt
@@ -0,0 +1,67 @@
+package com.herdi.yusli.glucosapp.customeView
+
+import android.content.Context
+import android.graphics.Canvas
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+
+class EmailEditText : AppCompatEditText, View.OnTouchListener {
+
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+    }
+
+    private fun init() {
+        setOnTouchListener(this)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                if (!isValidEmail(s.toString())) {
+                    error = "Format email xxx@xx.xxx"
+                }
+            }
+        })
+
+
+    }
+
+
+
+
+    override fun onTouch(v: View?, event: MotionEvent): Boolean {
+        return false
+    }
+
+    private fun isValidEmail(email: CharSequence): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+}
---
+++ GlucosApp/app/src/main/java/com/herdi/yusli/glucosapp/customeView/PasswordEditText.kt
@@ -0,0 +1,63 @@
+package com.herdi.yusli.glucosapp.customeView
+
+import android.content.Context
+import android.graphics.Canvas
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+
+class PasswordEditText : AppCompatEditText, View.OnTouchListener {
+
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+    }
+
+    private fun init() {
+        setOnTouchListener(this)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                if (s.toString().length < 6) {
+                    error = "password minimmal 6 karakter"
+                }
+            }
+        })
+
+
+    }
+
+
+
+
+    override fun onTouch(v: View?, event: MotionEvent): Boolean {
+        return false
+    }
+}
