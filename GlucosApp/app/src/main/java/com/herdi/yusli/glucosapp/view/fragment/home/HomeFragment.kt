1aadcfd91e5b76c478490ec60377fbc792fb34ce
herdi34 <herdi13yyo@gmail.com>
2022-05-23 17:40:03 +0700

    fix scroll in home fragment

--- GlucosApp/app/src/main/java/com/herdi/yusli/glucosapp/view/fragment/home/HomeFragment.kt
+++ GlucosApp/app/src/main/java/com/herdi/yusli/glucosapp/view/fragment/home/HomeFragment.kt
@@ -1,20 +1,20 @@
 package com.herdi.yusli.glucosapp.view.fragment.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.herdi.yusli.glucosapp.databinding.FragmentHomeBinding
+import com.herdi.yusli.glucosapp.view.LoginActivity
+
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -23,10 +23,18 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        binding.btnLogout.setOnClickListener {
+            val intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+
+
 
 
         return root
